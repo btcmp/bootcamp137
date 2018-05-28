8d2c2118735b697bbe7908160d92d366d8ae8e0c
Firmansyah <Firmansyah@rahloki>
2018-05-28 11:45:48 +0700

    edit relasi model event

--- src/main/java/com/newminiproject/model/Event.java
+++ src/main/java/com/newminiproject/model/Event.java
@@ -1,13 +1,16 @@
 package com.newminiproject.model;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
@@ -58,6 +61,12 @@ public class Event {
 	@Temporal(TemporalType.DATE)
 	private Date update_date;
 	private String updateBy;
+	
+	@OneToMany(fetch=FetchType.LAZY, mappedBy="tEventId")
+	private List<Design> listDesign;
+	
+	@OneToMany(fetch=FetchType.LAZY, mappedBy="event")
+	private List<Promotion> listPromotion;
 	
 	@ManyToOne
 	private Employee employee;
