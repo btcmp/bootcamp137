ff35e9e20727aeef5bc312b327f910c08714c235
User <User@DESKTOP-10LEGACY>
2018-05-28 10:31:36 +0700

    update controller employee

--- src/main/java/com/newminiproject/controller/EmployeeController.java
+++ src/main/java/com/newminiproject/controller/EmployeeController.java
@@ -9,17 +9,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import com.newminiproject.model.Company;
-import com.newminiproject.model.Employee;
-import com.newminiproject.service.CompanyService;
-import com.newminiproject.service.EmployeeService;
+import com.miniproject.xsis.model.Company;
+import com.miniproject.xsis.model.Employee;
+import com.miniproject.xsis.model.Souvenir;
+import com.miniproject.xsis.service.CompanyService;
+import com.miniproject.xsis.service.EmployeeService;
+
+
+
 
 @Controller
-@RequestMapping("employee")
+@RequestMapping("/employee")
 public class EmployeeController {
-	
 	@Autowired
 	EmployeeService employeeService;
 	@Autowired
@@ -28,16 +33,34 @@ public class EmployeeController {
 	@RequestMapping
 	public String index(Model model) {
 		List<Employee> listEmployee = employeeService.getAll();
-		List<Company> listCompany = companyService.getAll();
+		List<Company> listCompany= companyService.getAll();
 		model.addAttribute("listEmployee",listEmployee);
-		model.addAttribute("listCompany",listCompany);
-		return "employee";
+		model.addAttribute("listCompany", listCompany);
+		return "list-employee";
 	}
 	
+	
 	@RequestMapping(value="/save", method=RequestMethod.POST)
-	@ResponseStatus(HttpStatus.OK)
+	@ResponseStatus(HttpStatus.CREATED)
 	public void save(@RequestBody Employee employee) {
 		employeeService.save(employee);
-		
+	}
+	
+	@ResponseBody
+	@RequestMapping(value="/getemployee", method=RequestMethod.GET)
+	public Employee getEmployee(@RequestParam(value="id", required=false) int id) {
+		return  employeeService.getEmployeeById(id);
+	}
+	
+	@RequestMapping(value="/update", method= RequestMethod.PUT)
+	@ResponseStatus(HttpStatus.OK)
+	public void update(@RequestBody Employee employee) {
+		employeeService.update(employee);
+	}
+	
+	@RequestMapping(value="/delete", method=RequestMethod.DELETE)
+	@ResponseStatus(HttpStatus.OK)
+	public void delete(@RequestParam(value="id", required=false) int id) {
+		employeeService.delete(id);
 	}
 }
