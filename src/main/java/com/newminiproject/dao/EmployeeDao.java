de08e03739654e95fea5f067751a5f361508d068
User <User@DESKTOP-10LEGACY>
2018-05-28 10:39:00 +0700

    updated employee Dao

--- src/main/java/com/newminiproject/dao/EmployeeDao.java
+++ src/main/java/com/newminiproject/dao/EmployeeDao.java
@@ -2,12 +2,17 @@ package com.newminiproject.dao;
 
 import java.util.List;
 
-import com.newminiproject.model.Employee;
+import com.miniproject.xsis.model.Employee;
 
 public interface EmployeeDao {
 
-	void save(Employee employee);
+	public List<Employee> getAll() ;
 
-	List<Employee> getAll();
+	public void save(Employee employee);
 
+	public Employee getEmployeeByID(int id);
+
+	public void update(Employee employee);
+
+	public void delete(int id);
 }
