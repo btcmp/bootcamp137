2d53b99d1c336abcbd03f67154e89d15c5ff9f06
User <User@DESKTOP-10LEGACY>
2018-05-28 10:36:10 +0700

    update employee service

--- src/main/java/com/newminiproject/service/EmployeeService.java
+++ src/main/java/com/newminiproject/service/EmployeeService.java
@@ -1,29 +1,44 @@
 package com.newminiproject.service;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.newminiproject.dao.EmployeeDao;
-import com.newminiproject.model.Employee;
+import com.miniproject.xsis.dao.EmployeeDao;
+import com.miniproject.xsis.model.Employee;
 
 @Transactional
 @Service
 public class EmployeeService {
-	
 	@Autowired
 	EmployeeDao employeeDao;
+	
+	public List<Employee> getAll() {
+		return employeeDao.getAll();
+	}
 
 	public void save(Employee employee) {
 		// TODO Auto-generated method stub
 		employeeDao.save(employee);
 	}
 
-	public List<Employee> getAll() {
+	public Employee getEmployeeById(int id) {
 		// TODO Auto-generated method stub
-		return employeeDao.getAll();
+		return employeeDao.getEmployeeByID(id);
+	}
+
+	public void update(Employee employee) {
+		// TODO Auto-generated method stub
+		employee.setUpdatedDate(new Date());
+		employeeDao.update(employee);
+	}
+
+	public void delete(int id) {
+		// TODO Auto-generated method stub
+		employeeDao.delete(id);
 	}
 
 }
