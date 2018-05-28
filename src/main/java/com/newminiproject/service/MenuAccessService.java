d932b5073e2b014dd5bcc31b0aad441138b9ed36
Administrator <Administrator@PBP568K>
2018-05-28 10:14:14 +0700

    menambahkan user & menu access service

---
+++ src/main/java/com/newminiproject/service/MenuAccessService.java
@@ -0,0 +1,49 @@
+package com.newminiproject.service;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.miniproject.xsis.dao.MenuAccessDao;
+import com.miniproject.xsis.model.MenuAccess;
+
+@Service
+@Transactional
+public class MenuAccessService {
+	
+	@Autowired
+	MenuAccessDao maDao;
+	
+	public void save(MenuAccess menuAccess) {
+		// TODO Auto-generated method stub
+		menuAccess.setCreatedBy("Admin");
+		menuAccess.setCreatedDate(new Date());
+		maDao.save(menuAccess);
+	}
+
+	public List<MenuAccess> getAllMenuAccess() {
+		// TODO Auto-generated method stub
+		return maDao.getAllMenuAccess();
+	}
+
+	public void delete(MenuAccess menuAccess) {
+		// TODO Auto-generated method stub
+		maDao.delete(menuAccess);
+	}
+
+	public MenuAccess getMenuAccessById(int id) {
+		// TODO Auto-generated method stub
+		return maDao.getMenuAccessById(id);
+	}
+
+	public void update(MenuAccess menuAccess) {
+		// TODO Auto-generated method stub
+		menuAccess.setUpdatedBy("Admin");
+		menuAccess.setUpdatedDate(new Date());
+		maDao.update(menuAccess);
+	}
+
+}
---
+++ src/main/java/com/newminiproject/service/UserService.java
@@ -0,0 +1,59 @@
+package com.newminiproject.service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.miniproject.xsis.dao.RoleDao;
+import com.miniproject.xsis.dao.UserDao;
+import com.miniproject.xsis.model.Role;
+import com.miniproject.xsis.model.User;
+
+@Service
+@Transactional
+public class UserService {
+	
+	@Autowired
+	UserDao userDao;
+	
+	@Autowired
+	RoleDao roleDao;
+	
+	public List<User> getAll() {
+		// TODO Auto-generated method stub
+		return userDao.getAll();
+	}
+
+	public void save(User user) {
+		// TODO Auto-generated method stub
+		Role role= roleDao.getRoleById(user.getListRole().get(0).getId());
+		List<Role> listRole= new ArrayList();
+		listRole.add(role);
+		user.setListRole(listRole);
+		user.setCreatedBy("Administrator");
+		user.setCreatedDate(new Date());
+		
+		userDao.save(user);
+	}
+
+	public User getUserById(int id) {
+		// TODO Auto-generated method stubuser.getListRole().get(0).getId()
+		return userDao.getUserById(id);
+	}
+
+	public void update(User user) {
+		// TODO Auto-generated method stub
+		user.setUpdatedDate(new Date());
+		userDao.update(user);
+	}
+
+	public void delete(User usr) {
+		// TODO Auto-generated method stub
+		userDao.delete(usr);
+	}
+
+}
